9066de19677eb4fcf548a3b93f1043ae0a4bba4f
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2023-09-15 20:04:42 +0330

    improve: password validation

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,4 @@
-import {IsEmail, IsNotEmpty, IsString} from "class-validator";
+import {IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength} from "class-validator";
 
 export class AuthDto {
     @IsNotEmpty()
@@ -8,5 +8,8 @@ export class AuthDto {
 
     @IsNotEmpty()
     @IsString()
+    @MinLength(6, {message: 'Password must be at least 6 characters long'})
+    @MaxLength(20, {message: 'Password must not exceed 20 characters'})
+    @Matches(/^(?=.*[a-zA-Z])(?=.*\d)/, {message: 'Password must contain at least one letter and one number',})
     password: string;
 }
